0f8f853a09f953804929ed1dbc45516bd10a8ee1
Trivesh-Patel <triveshshivpatel@gmail.com>
2025-10-02 17:03:14 +0530

    Connections service, Api Gateway and Authentication added

---
+++ posts-service/src/main/java/com/triveshpatel/linkedin/posts_service/dto/PersonDto.java
@@ -0,0 +1,11 @@
+package com.triveshpatel.linkedin.posts_service.dto;
+
+import lombok.Data;
+
+@Data
+public class PersonDto {
+
+    private Long id;
+    private Long userId;
+    private String name;
+}
